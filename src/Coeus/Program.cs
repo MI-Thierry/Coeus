f2970dc98c6f6765658938fc5613f344a959785d
Muhirwa I. Thierry <mithierry1010@gmail.com>
2024-03-26 11:24:49 +0200

    Initial solution setup
    Installed and configured Blazorise for frontend development

---
+++ src/Coeus/Program.cs
@@ -0,0 +1,43 @@
+using Coeus.Client.Pages;
+using Coeus.Components;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+	.AddInteractiveServerComponents()
+	.AddInteractiveWebAssemblyComponents();
+
+// Add blazorise to the container
+builder.Services.AddBlazorise(options => options.Immediate = true)
+	.AddBootstrapProviders()
+	.AddFontAwesomeIcons();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseWebAssemblyDebugging();
+}
+else
+{
+	app.UseExceptionHandler("/Error", createScopeForErrors: true);
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+	.AddInteractiveServerRenderMode()
+	.AddInteractiveWebAssemblyRenderMode()
+	.AddAdditionalAssemblies(typeof(Coeus.Client._Imports).Assembly);
+
+app.Run();
